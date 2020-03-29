75bc76be221b8b681781f3b39f8fcb0360b365b9
Dmytro Karpovych <ZAYEC77@gmail.com>
2020-03-29 17:48:02 +0300

    improve login page

--- src/views/main/login.php
+++ src/views/main/login.php
@@ -8,6 +8,46 @@ use yii\helpers\Html;
 /** @var $model \nullref\fulladmin\models\LoginForm */
 
 $this->title = Yii::t('admin', 'Sign in');
+
+$this->registerCss(<<<CSS
+.toggle-password {
+    cursor: pointer;
+    user-select: none;
+}
+.field-login-form-rememberme {
+    padding-left: 5px;
+    user-select: none;
+}
+.field-login-form-rememberme input {
+    cursor: pointer;
+}
+CSS
+);
+
+$this->registerJs(<<<JS
+jQuery('.toggle-password').on('click', function() {
+    var i = jQuery(this).find('i');
+    i.toggleClass('glyphicon-eye-open').toggleClass('glyphicon-eye-close');
+    var input = jQuery(this).parent('.input-group').find('input');
+    var type = input.attr('type');
+    input.attr('type', type =='password' ? 'text': 'password')
+});
+JS
+);
+
+$passwordInputTemplate = <<<HTML
+<div class="input-group">
+    <span class="input-group-addon">
+        <i class="glyphicon glyphicon-lock"></i>
+    </span>
+    {input}
+    <span class="input-group-addon toggle-password">
+        <i class="glyphicon glyphicon-eye-open"></i>
+    </span>
+</div>
+{hint}
+{error}
+HTML;
 ?>
 <div class="main-login">
     <div class="container">
@@ -28,7 +68,7 @@ $this->title = Yii::t('admin', 'Sign in');
                             ]); ?>
 
                             <?= $form->field($model, 'password', [
-                                'template' => '<div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>{input}</div>{hint}{error}'
+                                'template' => $passwordInputTemplate,
                             ])->passwordInput([
                                 'placeholder' => ArrayHelper::getValue($model->attributeLabels(), 'password')
                             ]); ?>
